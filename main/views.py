a5305d159e16a737b37f93e658111ecaa15806af
Nekot94 <nekomato9@gmail.com>
2016-12-23 11:50:12 +0300

    init

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Product
+
+def home(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request,'home.html',context)
+
+def product(request, id):
+    product = Product.objects.get(id=id)
+    context = {'product': product}
+    return render(request,'product.html',context) 
